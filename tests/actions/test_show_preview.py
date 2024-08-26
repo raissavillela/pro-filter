3d84efb2dd7da773946e1c9f31696b71764df7ee
Raissa Villela <34253483+raissavillela@users.noreply.github.com>
2024-08-26 13:27:09 -0700

    Requisito 3

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,55 @@
-from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+from pro_filer.actions.main_actions import show_preview
+
+
+@pytest.fixture
+def context():
+    return {
+        "all_files": [
+            "src/__init__.py",
+            "src/app.py",
+            "src/utils/__init__.py",
+        ],
+        "all_dirs": ["src", "src/utils"],
+    }
+
+
+def test_show_preview_empty(capsys):
+    context = {"all_files": [], "all_dirs": []}
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == "Found 0 files and 0 directories\n"
+
+
+def test_show_preview_success_output(capsys, context):
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Found 3 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', "
+        "'src/utils/__init__.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
+
+
+def test_show_preview_more_than_five_files(capsys):
+    context = {
+        "all_files": [
+            "src/__init__.py",
+            "src/app.py",
+            "src/utils/__init__.py",
+            "src/utils/file1.py",
+            "src/utils/file2.py",
+            "src/utils/file3.py",
+        ],
+        "all_dirs": ["src", "src/utils"],
+    }
+    show_preview(context)
+    captured = capsys.readouterr()
+    assert captured.out == (
+        "Found 6 files and 2 directories\n"
+        "First 5 files: ['src/__init__.py', 'src/app.py', "
+        "'src/utils/__init__.py', 'src/utils/file1.py', "
+        "'src/utils/file2.py']\n"
+        "First 5 directories: ['src', 'src/utils']\n"
+    )
